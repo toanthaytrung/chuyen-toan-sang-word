20d07318be5d9f67adbf957cc20abb19a9536a8b
toanthaytrung <npdtrung2009@gmail.com>
2026-04-17 12:26:40 +0700

    Update model path for GenerativeModel initialization

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from pathlib import Path
 # Cấu hình API
 api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
-
+model = genai.GenerativeModel('models/gemini-1.5-flash') 
 def process():
     input_dir = Path('input')
     # Quét ảnh
